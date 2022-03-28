6c7458ed864ba3e200b2862b52bb70e7d4c08249
Alex Thiessen <alex.thiessen.de+github@gmail.com>
2022-03-29 00:49:20 +0200

    tests/acceptance/autojump: Ensure `jc` jumps to child dir

--- tests/acceptance/autojump_test.py
+++ tests/acceptance/autojump_test.py
@@ -5,6 +5,7 @@ import shlex
 import shutil
 import subprocess
 import sys
+from pathlib import Path
 
 sys.path.append(os.path.join(os.getcwd(), 'bin'))  # noqa
 
@@ -80,4 +81,74 @@ def test_autojump_returns_stable_results(tmpdir):
         f"foo__1__{join(tmpdir, 'foobarbar')}",
         f"foo__2__{join(tmpdir, 'foobar')}",
         f"foo__3__{join(tmpdir, 'foo')}",
+    ]
+
+
+def test_jc_jumps_to_child_match(tmpdir):
+    tmpdir = Path(tmpdir)
+    base = str(tmpdir)
+    bash = shutil.which('bash')
+
+    environment = os.environ
+    environment['HOME'] = base
+    environment['SHELL'] = bash
+
+    def install_subject():
+        subprocess.run(
+            ['make', 'install'], check=True, env=environment, capture_output=True,
+        )
+
+    subject_path = f'{os.getcwd()}/bin/autojump'
+    subject_initializer_path = f'{os.getcwd()}/bin/autojump.sh'
+
+    def autojump(*args):
+        shcmd = ' '.join(
+            [
+                'source',
+                shlex.quote(subject_initializer_path),
+                ';',
+                shlex.quote(subject_path),
+                ' '.join(map(str, args)),
+            ],
+        )
+
+        result = subprocess.run(
+            shcmd,
+            executable='/bin/bash',
+            env=environment,
+            capture_output=True,
+            shell=True,
+        )
+        if result.returncode == 0:
+            return result
+        else:
+            print()
+            print(f'stdout: {result.stdout}')
+            print(f'stderr: {result.stderr}')
+            result.check_returncode()
+
+    def add_to_db(path, weight_bump=0):
+        autojump('--add', path)
+        if weight_bump > 0:
+            try:
+                oldpwd = os.getcwd()
+                os.chdir(path)
+                autojump('--increase', weight_bump)
+            finally:
+                os.chdir(oldpwd)
+
+    install_subject()
+
+    add_to_db(_create(tmpdir / 'foo'), weight_bump=1)
+    add_to_db(_create(tmpdir / 'foo' / 'bar'), weight_bump=1)
+    add_to_db(_create(tmpdir / 'foo' / 'baz'), weight_bump=1)
+    add_to_db(_create(tmpdir / 'baz'))
+    add_to_db(_create(tmpdir / 'baz' / 'bar'))
+    add_to_db(_create(tmpdir / 'baz' / 'baz'))
+
+    run_results = autojump('--complete', str(tmpdir / 'baz'), 'bar')
+    run_lines = run_results.stdout.decode('utf-8').splitlines()
+
+    assert run_lines == [
+        f"bar__1__{str(tmpdir / 'baz' / 'baz')}",
     ]
